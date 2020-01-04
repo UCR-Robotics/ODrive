ee2f6c1220dcdf767861563cf820b1015e86ec06
KeranYe <38872265+KeranYe@users.noreply.github.com>
2020-01-03 20:39:42 -0800

    Updated code from Doggo
    Added New States for Dual Motor Control; Added Config Variables for Dual Motor Control;  Added Functions for Dual Motor Control; Added  Protocal Code for Dual Motor Control

--- Firmware/MotorControl/controller.hpp
+++ Firmware/MotorControl/controller.hpp
@@ -19,12 +19,23 @@ public:
         CTRL_MODE_CURRENT_CONTROL = 1,
         CTRL_MODE_VELOCITY_CONTROL = 2,
         CTRL_MODE_POSITION_CONTROL = 3,
-        CTRL_MODE_TRAJECTORY_CONTROL = 4
+        CTRL_MODE_TRAJECTORY_CONTROL = 4,        
+// Keran: Added New States for Dual Motor Control 
+/****************************************************/
+/** ADDED FROM DOGGO */
+        CTRL_MODE_COUPLED_CONTROL = 5,
+        CTRL_MODE_XY_CONTROL = 6
+/** ADDED FROM DOGGO */
+/****************************************************/
     };
 
     struct Config_t {
         ControlMode_t control_mode = CTRL_MODE_POSITION_CONTROL;  //see: Motor_control_mode_t
-        float pos_gain = 20.0f;  // [(counts/s) / counts]
+/****************************************************/
+/** ADDED FROM DOGGO */
+        float pos_gain = 0.01f;  // [(counts/s) / counts]
+/****************************************************/
+/** ADDED FROM DOGGO */
         float vel_gain = 5.0f / 10000.0f;  // [A/(counts/s)]
         // float vel_gain = 5.0f / 200.0f, // [A/(rad/s)] <sensorless example>
         float vel_integrator_gain = 10.0f / 10000.0f;  // [A/(counts/s * s)]
@@ -32,6 +43,25 @@ public:
         float vel_limit_tolerance = 1.2f;  // ratio to vel_lim. 0.0f to disable
         float vel_ramp_rate = 10000.0f;  // [(counts/s) / s]
         bool setpoints_in_cpr = false;
+
+// Keran: Added Config Variables for Dual Motor Control 
+/****************************************************/
+/** ADDED FROM DOGGO */
+        float kp_theta = 0.04f * 6000.0f / (2.0f * M_PI);
+        float kd_theta = 5.0f / 10000.0f * 6000.0f / (2.0f * M_PI);
+        float kp_gamma = 0.01f * 6000.0f / (2.0f * M_PI);
+        float kd_gamma = 5.0f / 10000.0f * 6000.0f / (2.0f * M_PI);
+
+        float kp_x = 0;
+        float kd_x = 0;
+        float kp_y = 0;
+        float kd_y = 0;
+
+        float gear_ratio = 3.0;
+/** ADDED FROM DOGGO */
+/****************************************************/        
+        
+        
     };
 
     explicit Controller(Config_t& config);
@@ -45,6 +75,17 @@ public:
     // Trajectory-Planned control
     void move_to_pos(float goal_point);
     void move_incremental(float displacement, bool from_goal_point);
+
+// Keran: Added Functions for Dual Motor Control 
+/****************************************************/
+/** ADDED FROM DOGGO */
+    void set_coupled_setpoints(float theta_setpoint, float gamma_setpoint);
+    void set_coupled_gains(float kp_theta, float kd_theta, float kp_gamma, float kd_gamma);
+    void set_xy_setpoints(float x_setpoint, float y_setpoint);
+    void set_xy_gains(float kp_x, float kd_x, float kp_y, float kd_y);
+    float encoder_to_rad(float x);
+/** ADDED FROM DOGGO */
+/****************************************************/    
     
     // TODO: make this more similar to other calibration loops
     void start_anticogging_calibration();
@@ -79,9 +120,18 @@ public:
     };
 
     Error_t error_ = ERROR_NONE;
+    
     // variables exposed on protocol
     float pos_setpoint_ = 0.0f;
     float vel_setpoint_ = 0.0f;
+    
+// Keran: Added  Protocal Code for Dual Motor Control 
+/****************************************************/
+/** ADDED FROM DOGGO */
+    float theta_ = 0.0f;
+    float gamma_ = 1.57f;
+/** ADDED FROM DOGGO */
+    
     // float vel_setpoint = 800.0f; <sensorless example>
     float vel_integrator_current_ = 0.0f;  // [A]
     float current_setpoint_ = 0.0f;        // [A]
@@ -91,6 +141,25 @@ public:
     uint32_t traj_start_loop_count_ = 0;
 
     float goal_point_ = 0.0f;
+    
+/** ADDED FROM DOGGO */
+    float theta_setpoint_ = 0.0f;
+    float gamma_setpoint_ = M_PI/2.0f;
+
+    float x_setpoint_ = 0.0f;
+    float y_setpoint_ = 0.13f;
+
+    float force_x_ = 0;
+    float force_y_ = 0;
+
+    float x_pos_ = 0;
+    float y_pos_ = 0;
+
+    float tau_theta_ = 0;
+    float tau_gamma_ = 0;
+
+    float J00=0.0f; float J01=0.0f; float J11=0.0f; float J10=0.0f;
+/** ADDED FROM DOGGO */    
 
     // Communication protocol definitions
     auto make_protocol_definitions() {
@@ -102,7 +171,29 @@ public:
             make_protocol_property("current_setpoint", &current_setpoint_),
             make_protocol_property("vel_ramp_target", &vel_ramp_target_),
             make_protocol_property("vel_ramp_enable", &vel_ramp_enable_),
+/** ADDED FROM DOGGO */            
+            make_protocol_property("theta_setpoint", &theta_setpoint_),
+            make_protocol_property("gamma_setpoint", &gamma_setpoint_),
+            make_protocol_property("x_setpoint", &x_setpoint_),
+            make_protocol_property("y_setpoint", &y_setpoint_),
+            make_protocol_property("force_x", &force_x_),
+            make_protocol_property("force_y", &force_y_),
+            make_protocol_property("x_pos", &x_pos_),
+            make_protocol_property("y_pos", &y_pos_),
+            make_protocol_property("tau_theta", &tau_theta_),
+            make_protocol_property("tau_gamma", &tau_gamma_),
+            make_protocol_property("J00", &J00),
+            make_protocol_property("J01", &J01),
+            make_protocol_property("J10", &J10),
+            make_protocol_property("J11", &J11),
+            make_protocol_property("theta", &theta_),
+            make_protocol_property("gamma", &gamma_),
+/** ADDED FROM DOGGO */
+            
             make_protocol_object("config",
+/** ADDED FROM DOGGO */                                 
+                make_protocol_property("gear_ratio", &config_.gear_ratio),                 
+/** ADDED FROM DOGGO */                                 
                 make_protocol_property("control_mode", &config_.control_mode),
                 make_protocol_property("pos_gain", &config_.pos_gain),
                 make_protocol_property("vel_gain", &config_.vel_gain),
@@ -111,6 +202,16 @@ public:
                 make_protocol_property("vel_limit_tolerance", &config_.vel_limit_tolerance),
                 make_protocol_property("vel_ramp_rate", &config_.vel_ramp_rate),
                 make_protocol_property("setpoints_in_cpr", &config_.setpoints_in_cpr)
+/** ADDED FROM DOGGO */                                 
+                make_protocol_property("kp_theta", &config_.kp_theta),
+                make_protocol_property("kd_theta", &config_.kd_theta),
+                make_protocol_property("kp_gamma", &config_.kp_gamma),
+                make_protocol_property("kd_gamma", &config_.kd_gamma),
+                make_protocol_property("kp_x", &config_.kp_x),
+                make_protocol_property("kd_x", &config_.kd_x),
+                make_protocol_property("kp_y", &config_.kp_y),
+                make_protocol_property("kd_y", &config_.kd_y)
+/** ADDED FROM DOGGO */                                 
             ),
             make_protocol_function("set_pos_setpoint", *this, &Controller::set_pos_setpoint,
                 "pos_setpoint", "vel_feed_forward", "current_feed_forward"),
@@ -120,10 +221,24 @@ public:
                                    "current_setpoint"),
             make_protocol_function("move_to_pos", *this, &Controller::move_to_pos, "pos_setpoint"),
             make_protocol_function("move_incremental", *this, &Controller::move_incremental, "displacement", "from_goal_point"),
+/** ADDED FROM DOGGO */
+            make_protocol_function("set_coupled_setpoints", *this, &Controller::set_coupled_setpoints,
+                "theta_setpoint",
+                "gamma_setpoint"),
+            make_protocol_function("set_xy_setpoints", *this, &Controller::set_xy_setpoints,
+                "x_setpoint",
+                "y_setpoint"),
+            make_protocol_function("set_xy_gains", *this, &Controller::set_xy_gains,
+                "kp_x",
+                "kd_x",
+                "kp_y",
+                "kd_y"),
+/** ADDED FROM DOGGO */
             make_protocol_function("start_anticogging_calibration", *this, &Controller::start_anticogging_calibration)
         );
     }
 };
+/****************************************************/
 
 DEFINE_ENUM_FLAG_OPERATORS(Controller::Error_t)
 
